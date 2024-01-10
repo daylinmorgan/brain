925b7361949c335ed8dc324ac28171f7144c6450
Daylin Morgan <daylinmorgan@gmail.com>
2024-01-10 12:59:30 -0600

    the era of nix is upon us

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "brain";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [zk hugo];
+          };
+        }
+    );
+}
