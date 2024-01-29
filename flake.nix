c1ee99585d45037b18fce08c0c753aca07ddbd88
Daylin Morgan <daylinmorgan@gmail.com>
2024-01-29 08:23:13 -0600

    no flake-utils

--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
   description = "brain";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
+  }: let
+    inherit (nixpkgs.lib) genAttrs;
+    forAllSystems = f:
+      genAttrs
+      ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]
+      (system: f nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (
+      pkgs:
         with pkgs; {
-          devShells.default = mkShell {
+          default = mkShell {
             buildInputs = [zk hugo nodePackages.pnpm];
           };
         }
     );
+  };
 }
