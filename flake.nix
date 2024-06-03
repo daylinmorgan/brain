992b0c5706e743d45cd643db30c665c3e9a3b723
Daylin Morgan <me@dayl.in>
2024-06-03 14:17:29 -0500

    specify nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,23 +1,28 @@
 {
   description = "brain";
 
-  outputs = {
-    nixpkgs,
-    ...
-  }: let
-    inherit (nixpkgs.lib) genAttrs;
-    forAllSystems = f:
-      genAttrs
-      ["x86_64-linux"] # "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]
-      (system: f nixpkgs.legacyPackages.${system});
-  in {
-    devShells = forAllSystems (
-      pkgs:
-        with pkgs; {
-          default = mkShell {
-            buildInputs = [zk hugo nodePackages.pnpm];
-          };
-        }
-    );
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+      forAllSystems =
+        f:
+        genAttrs [ "x86_64-linux" ] # "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]
+          (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zk
+            hugo
+            nodePackages.pnpm
+          ];
+        };
+      });
+    };
 }
