773e80278214f214dcd123afd5618b5f1c0005f8
Daylin Morgan <me@dayl.in>
2025-11-04 11:45:26 -0500

    switch to _:pkgs

--- flake.nix
+++ flake.nix
@@ -10,20 +10,22 @@
     let
       inherit (nixpkgs.lib) genAttrs;
       systems = [ "x86_64-linux" ]; # "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
-      forSystem = f: system: f (import nixpkgs { inherit system; });
+      forSystem = f: system: f system (import nixpkgs { inherit system; });
       forAllSystems = f: genAttrs systems (forSystem f);
     in
     {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # my syste zk is running a patched version
-            # zk
-            hugo
-            nodePackages.cspell
-            nodePackages.pnpm
-          ];
-        };
-      });
+      devShells = forAllSystems (
+        _: pkgs: {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # my syste zk is running a patched version
+              # zk
+              hugo
+              nodePackages.cspell
+              nodePackages.pnpm
+            ];
+          };
+        }
+      );
     };
 }
